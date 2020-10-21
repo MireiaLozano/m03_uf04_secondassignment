4c95f7d44dd2aeebe2fe3406df9287c762e10362
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-21 15:34:41 +0200

    Second assignment modificado

--- src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
@@ -12,18 +12,23 @@ package cat.copernic.m03_uf04_secondassignment;
 public class M03_UF04_SecondAssignment {
     
     public static void main(String[] args) {
-        Dispositivo sobremesa1 = new Ordenador("Lenovo", "Ordenador sobremesa con pantalla negra y torre de color negro", 1.200 );
+        Sobremesa sobremesa1 = new Ordenador("Lenovo", "Ordenador sobremesa con pantalla negra y torre de color negro", 1.200 );
         sobremesa1.conecta();
         sobremesa1.desconecta();
        
-        Dispositivo sobremesa2 = new TerminalVenta("NetPay Smart", "Dispositivo que en un establecimiento comercial, permite el cobro con targetas de crédito o débito", 39);
+        Sobremesa sobremesa2 = new TerminalVenta("NetPay Smart", "Dispositivo que en un establecimiento comercial, permite el cobro con targetas de crédito o débito", 39);
         sobremesa2.conecta();
         sobremesa2.desconecta();
         
-        Dispositivo mobil1 = new Tablet("Samsung Galaxy Tab A", "Tablet de la Gama Samsung A, disponible en blanco y gris, con 8 pulgadas de pantalla", 116.49);
+        Mobiles mobil1 = new Tablet("Samsung Galaxy Tab A", "Tablet de la Gama Samsung A, disponible en blanco y gris, con 8 pulgadas de pantalla", 116.49);
         mobil1.conecta();
         mobil1.desconecta();
         mobil1.reset();
+        
+        Mobiles mobil2 = new Portatil("HP", "Portatil HP mini, negro y plateado con teclado con iluminacion", 450);
+        mobil2.conecta();
+        mobil2.desconecta();
+        mobil2.reset();
     }
     
 }
