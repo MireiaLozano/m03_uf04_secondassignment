ff1c8239f73ed7ff346f6e9ed62b5dc3b4737942
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-21 15:40:11 +0200

    Ordenador hecha

---
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/Ordenador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cat.copernic.m03_uf04_secondassignment;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Ordenador extends Sobremesa {
+
+    public Ordenador(String nombre, String descripcion, double precio) {
+        super(nombre, descripcion, precio);
+    }
+    
+}
