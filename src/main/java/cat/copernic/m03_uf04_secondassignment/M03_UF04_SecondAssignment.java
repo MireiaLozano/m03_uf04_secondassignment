fdf27c3c4ca82c337cd6aa6796ade56ca1988ff8
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-21 15:45:55 +0200

    Second assignment con descripcion puesta en system out y acabado

--- src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
@@ -12,21 +12,25 @@ package cat.copernic.m03_uf04_secondassignment;
 public class M03_UF04_SecondAssignment {
     
     public static void main(String[] args) {
-        Sobremesa sobremesa1 = new Ordenador("Lenovo", "Ordenador sobremesa con pantalla negra y torre de color negro", 1.200 );
+        Sobremesa sobremesa1 = new Ordenador("Lenovo", "Ordenador sobremesa con pantalla negra y torre de color negro", 1200 );
         sobremesa1.conecta();
+        System.out.println(sobremesa1);
         sobremesa1.desconecta();
        
         Sobremesa sobremesa2 = new TerminalVenta("NetPay Smart", "Dispositivo que en un establecimiento comercial, permite el cobro con targetas de crédito o débito", 39);
         sobremesa2.conecta();
+        System.out.println(sobremesa2);
         sobremesa2.desconecta();
         
         Mobiles mobil1 = new Tablet("Samsung Galaxy Tab A", "Tablet de la Gama Samsung A, disponible en blanco y gris, con 8 pulgadas de pantalla", 116.49);
         mobil1.conecta();
+        System.out.println(mobil1);
         mobil1.desconecta();
         mobil1.reset();
         
         Mobiles mobil2 = new Portatil("HP", "Portatil HP mini, negro y plateado con teclado con iluminacion", 450);
         mobil2.conecta();
+         System.out.println(mobil2);
         mobil2.desconecta();
         mobil2.reset();
     }
