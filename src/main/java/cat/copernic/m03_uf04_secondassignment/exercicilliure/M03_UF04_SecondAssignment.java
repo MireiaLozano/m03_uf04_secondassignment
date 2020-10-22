b964dff074c5a9f9e53ec5eb92693ada2d6300c2
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-22 21:02:59 +0200

    Clase item

---
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/M03_UF04_SecondAssignment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cat.copernic.m03_uf04_secondassignment.exercicilliure;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class M03_UF04_SecondAssignment {
+    
+    public static void main(String[] args) {
+    }
+}
+    
