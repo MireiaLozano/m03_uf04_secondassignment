48ef2e9fd4273fcfa50064ec37e225e4ea485cc6
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-22 20:36:31 +0200

    Clase carrito

--- src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/CarritoDeLaCompra.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/CarritoDeLaCompra.java
@@ -45,7 +45,8 @@ public class CarritoDeLaCompra implements Finalizable {
 
     @Override
     public double checkout() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 0;
+        
     }
     
 }
