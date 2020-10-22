6bda31f6f9d05d4445e3f835a20599f06dc9034a
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-22 19:52:12 +0200

    1er intento clase item

--- src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Item.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Item.java
@@ -17,6 +17,58 @@ package cat.copernic.m03_uf04_secondassignment.exercicilliure;
  * 
  * 
  */
-public class Item {
+public abstract class Item extends CarritoDeLaCompra{
+    //Propiedades
+    private int idItem;
+    private String nombre;
+    private String descripcion;
+    private double precio;
     
+    //Constructor
+    public Item(int idItem, String nombre, String descripcion, double precio) {
+        this.idItem = idItem;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+    
+    //Getters/Setters
+
+    public int getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(int idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    //toString
+    @Override
+    public String toString() {
+        return "Item{" + "idItem=" + idItem + ", nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + '}';
+    }
 }
