d51377ca3c15ce994be4d1fce85d17468a595487
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-23 21:35:58 +0200

    Clases de diferentes tipos de item

---
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/Frescos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cat.copernic.m03_uf04_secondassignment.exercicilliure;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Frescos extends Item {
+
+    public Frescos(int idItem, String nombre, String descripcion, double precio) {
+        super(idItem, nombre, descripcion, precio);
+    } 
+ 
+}
