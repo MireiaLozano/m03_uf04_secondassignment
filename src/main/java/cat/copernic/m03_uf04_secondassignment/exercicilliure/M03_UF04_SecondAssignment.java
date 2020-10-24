b25d543add26ce23d94540844fb8317377224362
Mireia Lozano Sanchez <mireia.ls@terrassa.salesians.cat>
2020-10-24 23:51:02 +0200

    Cambios en clase second assginment, finalizada

--- src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/M03_UF04_SecondAssignment.java
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/exercicilliure/M03_UF04_SecondAssignment.java
@@ -21,17 +21,17 @@ public class M03_UF04_SecondAssignment {
         
         CarritoDeLaCompra carritoCompra = new CarritoDeLaCompra();
         
-           Detalle item1 = new Detalle(01, 15, new Alimentos(01, "Tomate", "Tomate Solís", 1.45));
+           Detalle item1 = new Detalle(01, 15, new Alimentos("Alimentos", 01, "Tomate", "Tomate Solís", 1.45));
            carritoCompra.añade(item1);
-           Detalle item2 = new Detalle(02, 02, new CuidadoPersonal("Perfumeria", 02, "Colonia mujer", "Colonia de mujer marca Playboy", 6.57));
+           Detalle item2 = new Detalle(02, 02, new CuidadoPersonal("Cuidado Personal", "Perfumeria", 02, "Colonia mujer", "Colonia de mujer marca Playboy", 6.57));
            carritoCompra.añade(item2);
-           Detalle item3 = new Detalle(03, 03, new Bebidas("Alcohol", 04, "Puerto de indias rosa", "Botella de 50cl de puerto de indias rosa", 7.5));
+           Detalle item3 = new Detalle(03, 03, new Bebidas("Bebidas", "Alcohol", 04, "Puerto de indias rosa", "Botella de 50cl de puerto de indias rosa", 7.5));
            carritoCompra.añade(item3);
-           Detalle item4 = new Detalle(04, 05, new Alimentos(05, "Pizza carbonara", "Pizza carbonaza marca Forno di pietra", 2.65));
+           Detalle item4 = new Detalle(04, 05, new Alimentos("Alimentos", 05, "Pizza carbonara", "Pizza carbonaza marca Forno di pietra", 2.65));
            carritoCompra.añade(item4);
-           Detalle item5 = new Detalle(05, 15, new Frescos(07, "San Jacobos", "San Jacobos de jamon y queso", 2.45));
+           Detalle item5 = new Detalle(05, 15, new Frescos("Frescos", 07, "San Jacobos", "San Jacobos de jamon y queso", 2.45));
            carritoCompra.añade(item5);
-           Detalle item6 = new Detalle(06, 01, new Tecnologia("Mobiles", 06, "Cargador mobil", "Cargador negro para Mobil Samsung Galaxy A10", 10));
+           Detalle item6 = new Detalle(06, 01, new Tecnologia("Tecnologia", "Mobiles", 06, "Cargador mobil", "Cargador negro para Mobil Samsung Galaxy A10", 10));
            carritoCompra.añade(item6);
           
            totalCompra=carritoCompra.checkout();
